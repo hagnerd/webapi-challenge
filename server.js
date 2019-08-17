ad1f6e277538a46e19cb73240d7289bb2b5fb7fc
Matt Hagner <web@matthagner.com>
2019-08-17 11:40:51 -0500

    Add json parsing middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const projects = require('./routes/projects');
-const projects = require('./routes/actions');
+const express = require("express");
+const projects = require("./routes/projects");
+const actions = require("./routes/actions");
 
 const server = express();
 
-server.use('/api/projects', projects);
-server.use('/api/actions', actions);
+server.use(express.json());
+
+server.use("/api/projects", projects);
+server.use("/api/actions", actions);
 
 module.exports = server;
